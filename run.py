6b12bbe9343b3ffd95ef42fb813113f632566eaf
bkzhn <absh.bktl@gmail.com>
2017-01-28 19:46:34 +0600

    add get_columns()

--- run.py
+++ run.py
@@ -16,5 +16,28 @@ def get_tables(connection):
     return tables
 
 
+def get_columns(connection, table):
+    """Function to get columns information."""
+    sql = 'DESCRIBE ' + table + ';'
+    result = connection.execute(sql)
+    columns = list()
+
+    for row in result:
+        column_name = row[0]
+        column_type = row[1].split('(')
+        column_type = column_type[0]
+
+        column = {
+            'name': column_name,
+            'type': column_type
+        }
+
+        columns.append(column)
+
+    return {
+        table: columns
+    }
+
+
 if __name__ == '__main__':
     print('== Simple Migrator ==')
