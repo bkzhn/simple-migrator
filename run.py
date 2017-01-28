6525265e6bbb76da8aa69763005d57f473055495
bkzhn <absh.bktl@gmail.com>
2017-01-28 19:44:01 +0600

    add get_tables()

--- run.py
+++ run.py
@@ -3,5 +3,18 @@ __author__ = 'bkzhn'
 
 connection_string = 'mysql+pymysql://root:1234@localhost/test'
 
+
+def get_tables(connection):
+    """Function to get all tables in db."""
+    sql = 'SHOW TABLES;'
+    result = connection.execute(sql)
+    tables = list()
+
+    for row in result:
+        tables.append(row[0])
+
+    return tables
+
+
 if __name__ == '__main__':
     print('== Simple Migrator ==')
